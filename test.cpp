7da17a5a101adbb12b2825d4781f99aa93d3d5f3
Qiancy1427 <qcy20231427@outlook.com>
2024-11-08 18:19:45 +0800

    new functions

--- test.cpp
+++ test.cpp
@@ -1,21 +1,19 @@
-//fuck ccf
 #include <fcntl.h>
 #include <termios.h>
 #include <unistd.h>
 #include <iostream>
 #include <cstring>
 using namespace std;
-int main() {
-    // 打开串口:ttyS0
-    int serialPort = open("/dev/ttyS0", O_RDWR | O_NOCTTY | O_NDELAY);
-    if (serialPort == -1) {
+int OpenSerial(string tty){
+    int serialport = open(tty, O_RDWR | O_NOCTTY | O_NDELAY);
+    if (serialport == -1) {
         std::cerr << "Error opening serial port" << std::endl;
-        return 1;
     }
-
-    // 配置串口参数
+    return serialport;
+}
+void SetSerial(int serialport){
     struct termios options;
-    tcgetattr(serialPort, &options);
+    tcgetattr(serialport, &options);
     cfsetispeed(&options, B9600); // 输入波特率
     cfsetospeed(&options, B9600); // 输出波特率
     options.c_cflag |= (CLOCAL | CREAD); // 启用接收器和本地模式
@@ -25,24 +23,30 @@ int main() {
     options.c_cflag &= ~CSTOPB; // 1个停止位
     options.c_cc[VMIN] = 1; // 最小读取字符数
     options.c_cc[VTIME] = 0; // 读取超时（以十分之一秒为单位）
-    tcsetattr(serialPort, TCSANOW, &options);
+    tcsetattr(serialport, TCSANOW, &options);
+}
+int main() {
+    int Input_serialPort = OpenSerial("/dev/ttyS0");
+    int Output_serialPort = OpenSerial("/dev/ttyS1")
+    SetSerial(Input_serialPort);
+    SetSerial(Output_serialPort);
 
     // 读取数据
     char buffer[128];
-    while(1){
-        memset(buffer,0,sizeof(buffer));
-        int bytesRead = read(serialPort, buffer, sizeof(buffer));
-        if (bytesRead > 0) {
-            cout << "Data read from serial port: " << string(buffer, bytesRead) << "\n";
-        } else {
-            cerr << "Error reading from serial port\n";
-            system("pause");
-        }
+    memset(buffer,0,sizeof(buffer));
+    int bytesRead = read(Input_serialPort, buffer, sizeof(buffer));
+    if (bytesRead > 0) {
+        cout << "Data read from serial port: " << string(buffer, bytesRead) << "\n";
+    } else {
+        cerr << "Error reading from serial port\n";
+        system("pause");
     }
+    int bytesWrite = write(Output_serialPort, buffer, sizeof(buffer));
 
 
     // 关闭串口
-    close(serialPort);
+    close(Input_serialPort);
+    close(Output_serialPort);
     //test redirect
     return 0;
 }
