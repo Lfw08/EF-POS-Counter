7215d8407508d12e76d6589fb49315e8ff9867b0
lfw08 <liufw666@outlook.com>
2024-12-24 13:19:24 +0800

    add GUI.py

---
+++ GUI.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk
+
+a="1145"
+
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text=a, font=("Arial", 400)).grid(column=0, row=1)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.mainloop()
